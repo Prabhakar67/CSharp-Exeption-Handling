f2d6cca3e3891238c1e5365069f289040be3bdc6
ahepra@2020spaces.com <Prabhakar.Aher2020spaces.com>
2022-12-01 12:04:08 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        var divider = 0;
+
+        try
+        {
+            try
+            {
+                var result = 100 / divider;
+            }
+            catch (NullReferenceException ex) // try removing parameters from bracket
+            {
+                Console.WriteLine("Inner catch");
+            }
+        }
+        catch
+        {
+            Console.WriteLine("Outer catch");
+        }
+        finally { Console.WriteLine("finally block executed"); }
+    }
+}
